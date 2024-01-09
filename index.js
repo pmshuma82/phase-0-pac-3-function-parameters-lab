5ea1e1ad6e45e268b3bbe0bd5a1e5c7ba9bfbc66
+ <+>
2024-01-09 06:42:42 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 function introduction(name) {
     return `Hi, my name is ${name}.`
 }
-function introductionWithLanguage(name) {language}
-{return `Hi, my name is [name] and I am learning to program in [language].`}
+function logTwoValues(name, language)    {
+    console.log(`The two values are ${name} and ${language}. `);
+    return `Hi, my name is (name) and I am learning to program in [language].`}
+
+    function sayHelloTo(firstName = "User") {
+        console.log(`Hello, ${firstName}!`);
+        
+    }
+
+function (introductionWithLanguageOptional){name, language}    {
+
+    return `Hi, my name is ${name} and I am learning to program in default ${language}.`
+function  ${name}
+
+}
